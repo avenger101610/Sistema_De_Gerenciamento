9a0c1ae892bec682369192d1a548cfe2a6f4c8f8
Tiago Dórea <tdorea@DORTECH.GROUP>
2023-12-19 11:22:10 -0300

    continuação do script de add tarefa

--- Script.js
+++ Script.js
@@ -19,5 +19,32 @@ function add(){
     labelTitle.innerHTML = 'Título da tarefa:'
     divTitle.appendChild(labelTitle)
 
+    var inpTitle = document.createElement('input') // <input type="text" class="txt" name="titletxt">
+    inpTitle.setAttribute('type', 'text')
+    inpTitle.setAttribute('class', 'txt')
+    inpTitle.setAttribute('name', 'titletxt')
+    divTitle.appendChild(inpTitle)
+
+    var descDiv = document.createElement('div') // <div class="flexcolumn">
+    descDiv.setAttribute('class', 'flexcolumn')
+    div.appendChild(descDiv)
+
+    var descLab = document.createElement('label') // <label for="desctxt" id="bloco">Descrição da Tarefa:</label>
+    descLab.setAttribute('for', 'desctxt')
+    descLab.setAttribute('id', 'bloco')
+    descLab.innerHTML = 'Descrição da Tarefa:'
+    descDiv.appendChild(descLab)
+
+    var descText = document.createElement('textarea') //<textarea type="textarea" class="txt2" name="desctxt"></textarea>
+    descText.setAttribute('type', 'textarea')
+    descText.setAttribute('class', 'txt2')
+    descDiv.appendChild(descText)
+
+    var radioDiv//<div class="alignLeft">
+
+
+
+
+
 
 }
